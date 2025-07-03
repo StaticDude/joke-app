0697a70359c3a526ef0bb0cda94cd484687905b5
o k <44340475-thestaticdude@users.noreply.replit.com>
2025-07-03 01:10:54 +0000

    On branch main  Changes to be committed: 	modified:   src/main/java/Main.java   .replit 	.project 	.settings/ 	pom.xml

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,44 @@
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.Border;
+import com.simtechdata.jokes.Jokes;
+import com.simtechdata.jokes.enums.Category;
+import com.simtechdata.jokes.enums.Flag;
 
 public class Main {
+
+    static Jokes jokes = new Jokes.Builder().addCategory(Category.PUN).addBlackList(Flag.EXPLICIT, Flag.NSFW, Flag.SEXIST, Flag.RACIST, Flag.POLITICAL, Flag.RELIGIOUS).build();
+    
     private static void createAndShowGUI() {
-        JFrame jFrame = new JFrame("Hello World Swing Example");
+        JFrame jFrame = new JFrame("App that generates jokes");
         jFrame.setLayout(new FlowLayout());
         jFrame.setSize(500, 360);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JLabel label = new JLabel("Hello World Swing");
-        Border border = BorderFactory.createLineBorder(Color.BLACK);
-        label.setBorder(border);
-        label.setPreferredSize(new Dimension(150, 100));
 
-        label.setText("Hello World Swing");
-        label.setHorizontalAlignment(JLabel.CENTER);
-        label.setVerticalAlignment(JLabel.CENTER);
+        JTextArea blague = new JTextArea(jokes.getAny());
+        blague.setEditable(false);
+        blague.setLineWrap(true);
+        blague.setWrapStyleWord(true);
+        blague.setPreferredSize(new Dimension(400, 200));
 
-        jFrame.add(label);
+        JButton refresh = new JButton("Refresh");
+        jFrame.add(blague);
+        refresh.addActionListener(e -> {
+            blague.setText(jokes.getAny());
+        });   
+        jFrame.add(refresh);
+
+
+
+
+
+        
         jFrame.setVisible(true);
     }
-    
-  public static void main(String[] args) {
-    createAndShowGUI();
-  }
+
+
+    public static void main(String[] args) {
+        createAndShowGUI();
+    }
 }
